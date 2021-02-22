4b2a80e4412543c7a3a4e079e761a7d1eab858b9
Collided Scope <collidedscope+github@protonmail.com>
2021-02-22 18:17:34 -0500

    Enable drawing a smaller, more aesthetically-pleasing cube
    `Cube#draw_mini` uses the Unicode box drawing characters ▀ and ▄ in
    order to render two "pixels" per row, which nicely squares up the
    otherwise elongated image we'd get in most terminal emulators, where
    cells are roughly twice as tall as they are wide.  These characters are
    notorious for producing small rendering artifacts, so fingers crossed.

--- lib/hecsa/cube/draw.cr
+++ lib/hecsa/cube/draw.cr
@@ -1,5 +1,6 @@
 class Hecsa::Cube
-  ART    = File.read File.expand_path "../../../data/art", __DIR__
+  ART    = File.expand_path "../../../data/art", __DIR__
+  MINI   = File.expand_path "../../../data/mini", __DIR__
   UFR    = "AaBbCcDd1IiJjKkLl3MmNnOoPp4"
   COLORS = [255, 208, 40, 196, 21, 226]
 
@@ -11,6 +12,35 @@ class Hecsa::Cube
       map[c.to_s] = "\e[48;5;#{color}m \e[0m"
     end
 
-    io.puts ART.gsub /./, map
+    io.puts File.read(ART).gsub /./, map
+  end
+
+  def draw_mini(io = STDOUT)
+    rows = [] of Array(Int32)
+
+    File.each_line(MINI) do |line|
+      rows << line.chars.map { |c|
+        c == ' ' ? 0 : COLORS[SOLVED.index(resolve(c.to_s)).not_nil! // 9]
+      }
+    end
+
+    rows.each_slice 2 do |(top, bot)|
+      io.puts String.build { |str|
+        top.zip(bot) { |t, b| str << render_pair t, b }
+      }
+    end
+  end
+end
+
+private def render_pair(t, b)
+  case {t, b}
+  when {0, 0}
+    ' '
+  when {_, 0}
+    "\e[38;5;%dm▀\e[m" % t
+  when {0, _}
+    "\e[38;5;%dm▄\e[m" % b
+  else
+    "\e[48;5;%d#{t == b ? "m " : ";38;5;%dm▀" % t}\e[m" % b
   end
 end
