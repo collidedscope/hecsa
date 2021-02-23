3efc3004d2d7422469fd20588b06297239946f5d
Collided Scope <collidedscope+github@protonmail.com>
2021-02-23 02:53:27 -0500

    Don't treat front-right slot F2L algs specially

--- lib/hecsa/knowledge/f2l.cr
+++ lib/hecsa/knowledge/f2l.cr
@@ -1,11 +1,5 @@
 require "yaml"
 
-vj = File.expand_path "../../../data/algs/Vj.yaml", __DIR__
-
-YAML.parse(File.open vj).as_h.each do |pair, alg|
-  Hecsa.teach_f2l pair.as_s, "Vj", alg.as_s
-end
-
 f2l = File.expand_path "../../../data/algs/F2L.yaml", __DIR__
 
 YAML.parse(File.open f2l).as_h.each do |pair, algs|
