9131a5fc9e4186651ec969e30a3653c856575431
Collided Scope <collidedscope+github@protonmail.com>
2021-02-26 09:14:44 -0500

    Consolidate runs of repeated moves in solution output

--- lib/hecsa/solver.cr
+++ lib/hecsa/solver.cr
@@ -119,8 +119,23 @@ module Hecsa
     end
 
     def show
-      p @solution
-      @cube.draw
+      p Solver.consolidate solution
+      @cube.draw_mini
+    end
+
+    # TODO: Make this not gross.
+    def self.consolidate(moves)
+      counts = {'\'' => -1, '2' => 2, '3' => 3}
+
+      moves
+        .chunks { |c| c[0] == 'U' || c[0] == 'y' }
+        .flat_map { |lost, seq|
+          next seq if !lost || seq.size == 1
+          seq.group_by(&.[0]).map { |face, moves|
+            degree = moves.sum { |move| counts[move[1]?]? || 1 } % 4
+            "#{face}#{"  2'"[degree]}" if degree > 0
+          }.compact
+        }
     end
 
     def self.log(msg, kind = nil, io = STDERR)
