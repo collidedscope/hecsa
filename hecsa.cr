ce998a52bb0b69add89dd6622781730634d78c2e
Collided Scope <collidedscope+github@protonmail.com>
2021-02-27 12:36:01 -0500

    Make HECSA find the best overall solution rather than just best cross

--- hecsa.cr
+++ hecsa.cr
@@ -1,19 +1,22 @@
 require "hecsa"
 
-def best_cross(scramble)
-  %w[\  z z2 z' x x'].product(%w[\  y y2 y']).map { |rot|
+def shortest_solution(scramble)
+  cross = %w[\  z z2 z' x x'].product %w[\  y y2 y']
+  f2l = %w[\  y y2 y'].product %w[\  U U2 U']
+
+  cross.product(f2l).map { |cross, f2l|
     Hecsa::Solver.new(scramble).tap { |s|
-      s.progress rot.join ' '
+      s.progress cross.join ' '
       s.cross
+      s.progress f2l.join ' '
+      s.f2l
+      s.oll
+      s.pll
     }
   }.min_by &.solution.size
 end
 
 print "Scramble: "
 if scramble = gets
-  sln = best_cross scramble
-  sln.f2l
-  sln.oll
-  sln.pll
-  sln.show
+  shortest_solution(scramble).show
 end
