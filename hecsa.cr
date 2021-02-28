4d20b87f904e59a4ce78d91de7e73bf460ae9a2e
Collided Scope <collidedscope+github@protonmail.com>
2021-02-28 15:01:55 -0500

    Update demo to use CFOPSolver

--- hecsa.cr
+++ hecsa.cr
@@ -1,11 +1,11 @@
-require "hecsa"
+require "hecsa/solvers/cfop"
 
 def shortest_solution(scramble)
   cross = %w[\  z z2 z' x x'].product %w[\  y y2 y']
   f2l = %w[\  y y2 y'].product %w[\  U U2 U']
 
   cross.product(f2l).map { |cross, f2l|
-    Hecsa::Solver.new(scramble).tap { |s|
+    Hecsa::CFOPSolver.new(scramble).tap { |s|
       s.progress cross.join ' '
       s.cross
       s.progress f2l.join ' '
