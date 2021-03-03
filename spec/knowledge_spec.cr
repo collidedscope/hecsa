374cc502e5934469687a2bf6278c39b388ced0d6
Collided Scope <collidedscope+github@protonmail.com>
2021-03-03 10:38:19 -0500

    Use a single Cube in specs courtesy of #undo

--- spec/knowledge_spec.cr
+++ spec/knowledge_spec.cr
@@ -3,10 +3,12 @@ require "hecsa/cube"
 require "hecsa/knowledge/*"
 
 describe Hecsa do
+  c = Hecsa::Cube.new
+
   it "should solve cross pieces" do
     Hecsa.cross_knowledge.each do |from, algs|
       algs.each do |to, alg|
-        Hecsa::Cube.new.exec(alg).resolve(to).should eq from
+        c.exec(alg) { c.resolve(to).should eq from }
       end
     end
   end
@@ -14,7 +16,7 @@ describe Hecsa do
   it "should solve F2L pairs" do
     Hecsa.f2l_knowledge.each do |from, algs|
       algs.each do |to, alg|
-        Hecsa::Cube.new.exec(alg).resolve(to).should eq from
+        c.exec(alg) { c.resolve(to).should eq from }
       end
     end
   end
