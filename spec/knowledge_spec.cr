7cb698522248a3bfdef384d0def3b545b122e57c
Collided Scope <collidedscope+github@protonmail.com>
2021-03-04 02:43:23 -0500

    Add specs to validate OLL and PLL algs

--- spec/knowledge_spec.cr
+++ spec/knowledge_spec.cr
@@ -1,5 +1,6 @@
 require "./spec_helper"
 require "hecsa/cube"
+require "hecsa/cube/cfop"
 require "hecsa/knowledge/*"
 
 describe Hecsa do
@@ -18,6 +19,22 @@ describe Hecsa do
       algs.each do |to, alg|
         c.exec(alg) { c.resolve(to).should eq from }
       end
+    end
+  end
+
+  it "should solve OLL cases" do
+    Hecsa.oll_knowledge.each_with_index do |alg, i|
+      c.undo alg
+      c.oll_case.not_nil![0].should eq i
+      c.exec alg
+    end
+  end
+
+  it "should solve PLL cases" do
+    Hecsa.pll_knowledge.each_with_index do |alg, i|
+      c.undo alg
+      c.pll_case.not_nil![0].should eq i
+      c.exec alg
     end
   end
 end
